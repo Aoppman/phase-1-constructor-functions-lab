3137b2e1ff2a6faa9fb2e592ad29db90851a62fe
Andrew <a.oppman@gmail.com>
2023-08-02 20:15:27 -0400

    added functions to index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+let Scooter = function (year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+};
+
+
+const Driver = function (name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+};
+
+
+let PickupLocation = function (address, city) {
+    this.address = address;
+    this.city = city;
+}
